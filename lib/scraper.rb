171a79cecf8aad95152cc0f51951f8a0338b0f48
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-21 04:13:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,4 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
- print doc.css(".headline-26OIBN").text
+print doc.css(".headline-26OIBN").text
